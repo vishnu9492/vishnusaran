04c49096a4366c7079e9835d2c3d0fa1edaa364f
NEELAPALA.VISHNU <NEELAPALA.VISHNU@LAPTOP-1LUBTN6T>
2019-05-16 10:53:48 +0530

    16-05-2019

---
+++ CORE JAVA/src/array/Multidemensional.java
@@ -0,0 +1,17 @@
+package array;
+
+public class Multidemensional {
+
+	public static void main(String[] args) {
+int a[][]= {{1,5,8,9},{4,5,8,9}};
+int b[][]= {{6,4,9,8},{5,8,9,10}};
+       int c[][]=new int[2][4];
+       for(int i=0;i<2;i++) {
+    	   for(int j=0;j<4;j++) {
+    	   c[i][j]=a[i][j]+b[i][j];
+    	  System.out.println(c[i][j]+"");
+    	  System.out.println();
+       }
+	}	
+
+}}
