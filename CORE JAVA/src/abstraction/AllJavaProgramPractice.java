a75c3349f5da2c2c31793638fa4784434cd115d7
NEELAPALA.VISHNU <NEELAPALA.VISHNU@LAPTOP-1LUBTN6T>
2019-06-18 15:28:03 +0530

    todays class

---
+++ CORE JAVA/src/abstraction/AllJavaProgramPractice.java
@@ -0,0 +1,21 @@
+package abstraction;
+
+import java.util.Scanner;
+
+public class AllJavaProgramPractice {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a number");
+	int s=sc.nextInt();
+	System.out.println("Enter another numder");
+	int k=sc.nextInt();
+	int y=s*k;
+	System.out.println(y);
+	
+			
+		}
+
+	}
+
+}
